509077534040e64c99aa74fa47feaa56ec16451f
Peter Adegboyega <peter.gboyega@yahoo.com>
2024-04-21 23:10:23 +0100

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Accordian from './components';
+
+function App() {
+  return (
+    <div className="App">
+    <Accordian/>
+    </div>
+  );
+}
+
+export default App;
